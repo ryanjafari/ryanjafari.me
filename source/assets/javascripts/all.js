f8bc6a55865943c8ddf3ad205e7916d6e79414ec
Ryan Jafari <ryan@cetrasoft.com>
2014-09-13 15:14:34 -0400

    Add ga event for halfway reads
    Firing an event off when the visitor scrolls halfway through the article
    content will tell GA to not count the visit as a bounce if that visitor
    ends up not visiting any another links.
    
    Recommended suggestions from GA on setting a timeout were pretty shitty
    compared to going this route, inspired from a blog post on LinkedIn.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -4,11 +4,25 @@
 //= require ekko-lightbox/dist/ekko-lightbox
 
 $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
+  event.preventDefault();
+  $(this).ekkoLightbox();
 });
+
+var navbar_height;
+var content_area_height;
+var halfway_mark;
 
 $(document).on('ready', function(event) {
   $('[data-toggle=tooltip]').tooltip();
+  navbar_height = $('[data-measure=navbar]').outerHeight(true);
+  content_area_height = $('[data-measure=content-area]').height();
+  halfway_mark = (content_area_height) / 2;
 });
 
+$(window).scroll(function() {
+  var window_scroll_pos = $(this).scrollTop();
+  if (window_scroll_pos + navbar_height >= halfway_mark) {
+    ga('send', 'event', 'halfway mark', 'read');
+    $(window).unbind('scroll');
+  }
+});
